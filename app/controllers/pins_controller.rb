d72d19ad970c0d526e47e4fa0b2e2c9c76471883
Naren Kukreja <narenkukreja@gmail.com>
2016-02-17 18:55:46 -0500

    Added image attachment options using paperclip gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -46,7 +46,7 @@ class PinsController < ApplicationController
   private
 
   def pin_params
-    params.require(:pin).permit(:title, :description)
+    params.require(:pin).permit(:title, :description, :image)
   end
 
   def find_pin
